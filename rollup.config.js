8600ff0103d4426c1aa7822e157c18f15dc94a26
Jesse Sivonen <j.sivonen@hotmail.com>
2019-03-23 10:17:18 +0200

    Change webpack to rollup

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+
+
+import { readdirSync } from 'fs';
+
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs' 
+import VuePlugin from 'rollup-plugin-vue';
+import replace from 'rollup-plugin-replace';
+
+const mode = 'development';
+const src = './src/frontend';
+
+export default {
+    input: readdirSync(src + '/pages')
+            .filter((filename) => /\.vue$/.test(filename))
+            .map((filename) => src + '/pages/' + filename)
+            .concat([src + '/app.js']),
+    output: {
+        dir: 'public/assets',
+        format: 'esm',
+    },
+    plugins: [
+        replace({
+            'process.env.NODE_ENV': JSON.stringify(mode),
+        }),
+        resolve(),
+        commonjs(),
+        VuePlugin(),
+    ],
+};
